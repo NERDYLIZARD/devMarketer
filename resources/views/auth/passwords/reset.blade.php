1d6571bc11fe17435f02505d3d6578024ebb0123
NERDYLIZARD <bouasavanhhop@gmail.com>
2017-07-14 08:25:53 +0530

    refactor: partial files

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -14,7 +14,7 @@
         <div class="card-content">
           <h1 class="title">Reset Your Password</h1>
 
-          <form action="" method="POST" role="form">
+          <form action="{{route('password.reset')}}" method="POST" role="form">
             {{csrf_field()}}
             <input type="hidden" name="token" value="{{ $token }}">
             <div class="field">
