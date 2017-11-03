30504ef541d2740e2c0f6e3e01b3261ac3c28b4a
NERDYLIZARD <bouasavanhhop@gmail.com>
2017-11-03 14:45:43 +0530

    post - create page: Simple UI

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+      return view('manage.posts.index');
     }
 
     /**
@@ -24,7 +24,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+      return view('manage.posts.create');
     }
 
     /**
